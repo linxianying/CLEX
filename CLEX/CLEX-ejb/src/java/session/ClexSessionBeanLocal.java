74327e8db48dbb0c153384c07a08b082521d5779
eeren <borderline1993@hotmail.com>
2018-02-18 02:56:04 +0800

    Revert "added "findGuest", implemented basic login functions, created new main page for student login"
    This reverts commit 9a76bd53e53904c6ec97db177df529d7917c9561.

--- CLEX/CLEX-ejb/src/java/session/ClexSessionBeanLocal.java
+++ CLEX/CLEX-ejb/src/java/session/ClexSessionBeanLocal.java
@@ -7,7 +7,6 @@ package session;
 
 import entity.Admin;
 import entity.Course;
-import entity.Guest;
 import entity.Lecturer;
 import entity.Module;
 import entity.Student;
@@ -25,25 +24,20 @@ public interface ClexSessionBeanLocal {
 
     public void createStudent(String username, String password, String name, String email, String school, Long contactNum, String salt,
                 String faculty, String major, String matricYear, String matricSem, String currentYear, double cap);
-    
     public boolean checkNewUser(String username);
     public boolean checkNewCourse(String moduleCode);
     public boolean checkNewAdmin(String username);
     public boolean checkNewLecturer(String username);
     public boolean checkNewStudent(String username);
-    
     /*public String viewModule(String moduleCode); */
     public boolean updateStudentEmail(String username, String newEmail);
     public boolean updateStudentFaculty(String username, String faculty);
-    public boolean updateStudentContact(String username, Long contactNum); 
-    
     public boolean checkPassword(String username, String password);
     public String removeTask(Long taskId);
-    
+    public boolean updateStudentContact(String username, Long contactNum);
     public Student findStudent(String username);
     public Lecturer findLecturer(String username);
     public Admin findAdmin(String username);
-    public Guest findGuest(String username);
     public Module findModule(String moduleCode, String takenYear, String takenSem);
     public Course findCourse(String moduleCode);
     public Task findTask(Long taskId);
--- CLEX/CLEX-war/src/java/managedbeans/LoginBean.java
+++ CLEX/CLEX-war/src/java/managedbeans/LoginBean.java
@@ -10,21 +10,16 @@ package managedbeans;
 
 import entity.User;
 import java.io.IOException;
-import java.io.Serializable;
 import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import session.ClexSessionBeanLocal;
 
 /**
  *
  * @author eeren
  */
-@RequestScoped
-@ManagedBean
-public class LoginBean implements Serializable{
+@Named(value = "loginBean")
+public class LoginBean {
     
     @EJB
     private ClexSessionBeanLocal csbl;
@@ -32,12 +27,7 @@ public class LoginBean implements Serializable{
     private User userEntity;
     private String username;
     private String password;
-    private String userType;
 
-    public LoginBean(){
-        
-    }
-    
     public User getUserEntity() {
         return userEntity;
     }
@@ -62,59 +52,8 @@ public class LoginBean implements Serializable{
         this.password = password;
     }
 
-    public String getUserType() {
-        return userType;
-    }
-
-    public void setUserType(String userType) {
-        this.userType = userType;
-    }
-
-    public void doLogin() throws IOException{
-        FacesMessage fmsg = new FacesMessage();
-        //Login based on usertype first, then check username, then password
-        if(userType.equals("1")){ //Student
-            userEntity = csbl.findStudent(username);
-            if(userEntity == null){
-               fmsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "User '" + username + "' does not exists.", "");
-               FacesContext.getCurrentInstance().addMessage(null, fmsg);
-            }
-            else{
-                if(csbl.checkPassword(username, password)){
-                    FacesContext.getCurrentInstance().getExternalContext().redirect("studentMain.xhtml");
-                }
-                else{
-                    fmsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Incorrect password.", "");
-                    FacesContext.getCurrentInstance().addMessage(null, fmsg);
-                }
-            }
-        }
-        else if(userType.equals("2")){ //Lecturer
-            userEntity = csbl.findLecturer(username);
-            if(userEntity == null){
-               
-            }
-            else{
-                
-            }
-        }
-        else if(userType.equals("3")){ //Admin
-            userEntity = csbl.findAdmin(username);
-            if(userEntity == null){
-               
-            }
-            else{
-                
-            }
-        }
-        else{ //Guest
-            userEntity = csbl.findGuest(username);
-            if(userEntity == null){
-               
-            }
-            else{
-                
-            }
-        }
+    
+    public boolean login() throws IOException{
+        return csbl.checkPassword(username, password);
     }
 }
