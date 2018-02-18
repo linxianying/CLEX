aa5b61b71684d6842ab98e7016defcb4d14e3a27
linxianying <soullxy@qq.com>
2018-02-18 20:54:47 +0800

    add managed bean corresponding to the added xhtml pages

---
+++ CLEX/CLEX-war/src/java/managedbeans/ProjectBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author lin
+ */
+@ManagedBean
+@RequestScoped
+public class ProjectBean {
+
+    /**
+     * Creates a new instance of ProjectBean
+     */
+    public ProjectBean() {
+    }
+    
+}
