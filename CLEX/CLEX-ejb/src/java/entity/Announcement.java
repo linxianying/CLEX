476b2e7432e57e3e1b0140625c72dd1906a67f4e
eeren <borderline1993@hotmail.com>
2018-03-01 16:41:02 +0800

    added backend for announcement + entity, updated layout for course mgmt (admin), fixed filter for user list

---
+++ CLEX/CLEX-ejb/src/java/entity/Announcement.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author eeren
+ */
+@Entity
+public class Announcement implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String title;
+    private String message;
+    private String createdDate;
+    private String type; //Input "admin": admin announcement, "<modulecode>": lecturer announcement
+    private String audience; //Input "1": all, "2": students only, "3": lecturers only, "4": guests only, "5": 2&3 (admin can use any, lecturer's default use 2)
+    
+    @ManyToOne
+    private User user;
+    
+    public Announcement(){
+        Date current = new Date();
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        this.createdDate = format.format(current);
+    }
+    
+    public void lecturerCreate(String title, String message, String type){
+        this.title = title;
+        this.message = message;
+        this.type = type;
+        this.audience = "2";
+    }
+    
+    public void adminCreate(String title, String message, String audience){
+        this.title = title;
+        this.message = message;
+        this.type = "admin";
+        this.audience = audience;
+    }    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAudience() {
+        return audience;
+    }
+
+    public void setAudience(String audience) {
+        this.audience = audience;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Announcement)) {
+            return false;
+        }
+        Announcement other = (Announcement) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Announcement[ id=" + id + " ]";
+    }
+    
+}
---
+++ CLEX/CLEX-war/src/java/managedbeans/AnnouncementBean.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import entity.User;
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import session.AnnouncementSessionBeanLocal;
+
+/**
+ *
+ * @author eeren
+ */
+@ManagedBean
+@RequestScoped
+public class AnnouncementBean {
+
+    @EJB
+    AnnouncementSessionBeanLocal asbl;
+    
+    private User userEntity;
+    private String username;
+    private int userType;
+    
+    private Long anncId;
+    private String title;
+    private String message;
+    private String type; //Input "admin": admin announcement, "<modulecode>": lecturer announcement
+    private String audience; //Input "1": all, "2": students only, "3": lecturers only, "4": guests only, "5": 2&3 (admin can use any, lecturer's default use 2)
+
+    FacesContext context;
+    HttpSession session;
+    
+    public AnnouncementBean() {
+    }
+    
+    public void enterAnnouncement() throws IOException{
+        FacesMessage fmsg = new FacesMessage();
+        context = FacesContext.getCurrentInstance();
+        session = (HttpSession) context.getExternalContext().getSession(true);
+        userEntity = (User) session.getAttribute("user");
+        username = (String) session.getAttribute("username");
+        userType = (int) session.getAttribute("userType");
+        
+        if(userType == 2){
+            asbl.createLecturerAnnc(username, title, message, type);
+            fmsg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Announcement created.", "");
+            context.addMessage(null, fmsg);           
+            context.getExternalContext().redirect("lecturerMain.xhtml");
+        }
+        else if(userType == 3){
+            asbl.createAdminAnnc(username, title, message, audience);
+            fmsg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Announcement created.", "");
+            context.addMessage(null, fmsg);  
+            context.getExternalContext().redirect("adminMain.xhtml");
+        }
+        else{
+            fmsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error creating announcement.", "");
+            context.addMessage(null, fmsg);
+        }
+    }  
+    
+    public User getUserEntity() {
+        return userEntity;
+    }
+
+    public void setUserEntity(User userEntity) {
+        this.userEntity = userEntity;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+
+    public Long getAnncId() {
+        return anncId;
+    }
+
+    public void setAnncId(Long anncId) {
+        this.anncId = anncId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAudience() {
+        return audience;
+    }
+
+    public void setAudience(String audience) {
+        this.audience = audience;
+    }
+    
+    
+}
