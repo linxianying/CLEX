7ca7f01ea6b2765a6485cb347ba9aef102b288f3
eeren <borderline1993@hotmail.com>
2018-03-02 14:24:04 +0800

    added function for announcement

--- CLEX/CLEX-war/src/java/managedbeans/AnnouncementBean.java
+++ CLEX/CLEX-war/src/java/managedbeans/AnnouncementBean.java
@@ -7,6 +7,7 @@ package managedbeans;
 
 import entity.User;
 import java.io.IOException;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
