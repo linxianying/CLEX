01f8a1175cb08092b47a4b74237028027be1b6be
eeren <borderline1993@hotmail.com>
2018-03-02 15:35:19 +0800

    created profile session beans for editing profile

---
+++ CLEX/CLEX-ejb/src/java/session/ProfileSessionBeanLocal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author eeren
+ */
+@Local
+public interface ProfileSessionBeanLocal {
+
+    public void editStudent(String username, String name, String email, String school, Long contactNum, String faculty, String major, String matricYear, String matricSem, double cap);
+    public void editLecturer(String username, String name, String email, String school, Long contactNum, String faculty);
+    public void editGuest(String username, String name, String email, String school, Long contactNum);
+    
+    public void changePassword(String username, String newPassword);
+    public boolean checkPassword(String username, String password);
+}
