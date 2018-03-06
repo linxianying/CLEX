799c2c8ca732fb48c66725fa2162757ac2f39041
Jeffrey <jeffrey0612@gmail.com>
2018-03-06 20:16:40 +0800

    -added day and time list in CourseMgmtBean for frontend display -added retrevial functions in CourseBean init() -updated layout for Admin Course Management Page: dropdown lists -updated layout for Lecturer Module Management Page: dropdown lists

--- CLEX/CLEX-ejb/src/java/session/CourseMgmtBeanLocal.java
+++ CLEX/CLEX-ejb/src/java/session/CourseMgmtBeanLocal.java
@@ -5,9 +5,9 @@
  */
 package session;
 
-import entity.Course;
 import entity.Lesson;
 import entity.Module;
+import entity.User;
 import java.util.Collection;
 import java.util.List;
 import javax.ejb.Local;
@@ -18,34 +18,56 @@ import javax.ejb.Local;
  */
 @Local
 public interface CourseMgmtBeanLocal {
-    public void createCourse(String moduleCode, String moduleName, String moduleInfo , boolean discontinuedBool,
-        String discountinuedYear, String discountinuedSem, String offeredSem, String school, String moduleCredit, String workload);
+
+    public void createCourse(String moduleCode, String moduleName, String moduleInfo, boolean discontinuedBool,
+            String discountinuedYear, String discountinuedSem, String offeredSem, String school, String moduleCredit, String workload);
+
     public void createModule(String takenYear, String takenSem, String prerequisite, String preclusions, String moduleCode);
+
     public void createLesson(String day, String timeFrom, String timeEnd, String type, String venue, String moduleCode, String takenYear, String takenSem);
-    
+
     public List getAllCourses();
+
     public List getAllModules();
+
     public List getAllLessons();
-    
-    public void editCourse(String moduleCode, String moduleName, String moduleInfo , boolean discontinuedBool,
-        String discountinuedYear, String discountinuedSem, String offeredSem, String school, String moduleCredit, String workload);
+
+    public void editCourse(String moduleCode, String moduleName, String moduleInfo, boolean discontinuedBool,
+            String discountinuedYear, String discountinuedSem, String offeredSem, String school, String moduleCredit, String workload);
+
     public void editModule(String takenYear, String takenSem, String prerequisite, String preclusions, String moduleCode);
+
     public void editLesson(String day, String timeFrom, String timeEnd, String type, String venue, String moduleCode, String takenYear, String takenSem);
-    
+
     public boolean deleteCourse(String moduleCode);
+
     public boolean deleteModule(String moduleCode, String takenYear, String takenSem);
+
     public boolean deleteLesson(String day, String timeFrom, String timeEnd, String moduleCode, String takenYear, String takenSem);
-    
+
     public boolean checkNewCourse(String moduleCode);
+
     public boolean checkExistingCourse(String moduleCode);
+
     public boolean checkNewModule(String moduleCode, String takenYear, String takenSem);
+
     public boolean checkExistingModule(String moduleCode, String takenYear, String takenSem);
+
     public boolean checkNewLesson(String moduleCode, String takenYear, String takenSem, String day, String timeFrom, String timeEnd);
+
     public boolean checkExistingLesson(String moduleCode, String takenYear, String takenSem, String day, String timeFrom, String timeEnd);
-    
+
     public boolean linkLecturerToModule(String moduleCode, String takenYear, String takenSem, String username);
+
     public boolean checkLectTeachModule(String username, String moduleCode, String takenYear, String takenSem);
-    
+
     public Collection<Module> getModulesFromLecturer(String username);
+
     public Collection<Lesson> getLessonsFromLecturer(String username);
+
+    public List getAllTimings();
+
+    public List getAllDays();
+
+    public Collection<User> getLecturerName();
 }
