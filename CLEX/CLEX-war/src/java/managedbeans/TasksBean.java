fa0803edd1b42e1c84f1328cb6fa8dfdede2a80b
CaoYu <e0012696@u.nus.edu>
2018-03-23 00:04:50 +0800

    minor change

--- CLEX/CLEX-war/src/java/managedbeans/TasksBean.java
+++ CLEX/CLEX-war/src/java/managedbeans/TasksBean.java
@@ -20,6 +20,7 @@ import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -33,7 +34,7 @@ import session.ToDoListSessionBeanLocal;
  *
  * @author lin
  */
-@Named(value = "tasksBean")
+@ManagedBean(name = "tasksBean")
 @SessionScoped
 public class TasksBean {
 
@@ -73,6 +74,7 @@ public class TasksBean {
         studentEntity = (Student) session.getAttribute("user");
         username = studentEntity.getUsername();
         
+        System.out.println("finish intialization");
     }
     
     
