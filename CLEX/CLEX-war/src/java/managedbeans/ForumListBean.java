8be75ffde32b3669fac500b400625d1f6abc669b
Jeffrey <jeffrey0612@gmail.com>
2018-04-07 23:47:23 +0800

    -fixed forms not clearing after creating new thread/reply and search

--- CLEX/CLEX-war/src/java/managedbeans/ForumListBean.java
+++ CLEX/CLEX-war/src/java/managedbeans/ForumListBean.java
@@ -155,7 +155,9 @@ public class ForumListBean {
                 fmsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failed to create thread.", "Please ensure you are logged in.");
             }
         }
-
+        tTitle = "";
+        tContent = "";
+        tTag = "";
         context.addMessage(null, fmsg);
     }
 
@@ -164,17 +166,14 @@ public class ForumListBean {
         try {
             int usertype = (int) session.getAttribute("userType");
             session.setAttribute("id", selectedThread.getId());
-            if(usertype == 1){ //Student
-                context.getExternalContext().redirect("viewThread.xhtml");                
-            }
-            else if(usertype == 2){ //Lecturer
-                context.getExternalContext().redirect("viewThreadL.xhtml");  
-            }
-            else if(usertype == 3){ //Admin
-                context.getExternalContext().redirect("viewThreadA.xhtml");  
-            }
-            else if(usertype == 4){ //Guest
-                context.getExternalContext().redirect("viewThreadG.xhtml");  
+            if (usertype == 1) { //Student
+                context.getExternalContext().redirect("viewThread.xhtml");
+            } else if (usertype == 2) { //Lecturer
+                context.getExternalContext().redirect("viewThreadL.xhtml");
+            } else if (usertype == 3) { //Admin
+                context.getExternalContext().redirect("viewThreadA.xhtml");
+            } else if (usertype == 4) { //Guest
+                context.getExternalContext().redirect("viewThreadG.xhtml");
             }
         } catch (Exception e) {
             e.printStackTrace();
