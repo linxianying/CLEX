6a0b8e71f4344cc887571bd9cf4743bd514b1f39
Jeffrey <jeffrey0612@gmail.com>
2018-04-08 08:59:18 +0800

    -added basic whiteboard for student (still in development) -to add: student export drawn image for lecturer to view

---
+++ CLEX/CLEX-war/src/java/managedbeans/WhiteboardBean.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author lin
+ */
+@ManagedBean
+@SessionScoped
+public class WhiteboardBean {
+
+    FacesContext context;
+    HttpSession session;
+
+    private String currentDrawing;
+    private String previousDrawing;
+
+    public WhiteboardBean() {
+    }
+
+    public void init() {
+
+    }
+
+    public void undo() {
+        currentDrawing = previousDrawing;
+    }
+
+    public void onInput(ValueChangeEvent e) {
+        
+        previousDrawing = currentDrawing;
+        currentDrawing = e.getNewValue().toString();
+        
+    }
+
+    public void cleardrawing() {
+        System.out.println("pre before: " + previousDrawing);
+        System.out.println("curr before: " + currentDrawing);
+        currentDrawing = "";
+        previousDrawing = "";
+        System.out.println("pre after: " + previousDrawing);
+        System.out.println("curr after: " + currentDrawing);
+    }
+
+    public String getCurrentDrawing() {
+        return currentDrawing;
+    }
+
+    public void setCurrentDrawing(String currentDrawing) {
+        this.currentDrawing = currentDrawing;
+    }
+
+    public String getPreviousDrawing() {
+        return previousDrawing;
+    }
+
+    public void setPreviousDrawing(String previousDrawing) {
+        this.previousDrawing = previousDrawing;
+    }
+}
